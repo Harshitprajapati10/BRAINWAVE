472a86dcc6fb49ddbe6203598f4218047e6dcf5b
Harshit Prajapati <143312448+Harshitprajapati10@users.noreply.github.com>
2024-09-18 09:22:00 +0000

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,58 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { premiumH } from "./assets";
 
 const App = () => {
+  const [showImage, setShowImage] = useState(true);
+  const [width, setWidth] = useState("0%");
+  useEffect(() => {
+    // Set a timeout to change width after the page loads
+    const timer1 = setTimeout(() => {
+      setWidth("100%");
+    }, 500);
+
+    return () => clearTimeout(timer1); // Clean up the timer
+  }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // setWidth("100%");
+      setShowImage(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Header />
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
+      {showImage && (
+      <div className="h-[95vh] w-[100vw]">
+        <div className="h-[10px] bg-purple-900 transition-all duration-500" style={{ width }}></div>
+        <div className="w-full h-[80vh] flex items-center justify-center">
+          <img className="h-[20vh] rotate-340" src={premiumH} alt="Loading..." />
+        </div>
+        <div className="w-full flex justify-center">
+          <p className=" text-5xl font-edu-vic-wa-nt-beginner font-bold">
+            premiumH
+          </p>
+        </div>
       </div>
-      <ButtonGradient />
-      <Footer />
+     )} 
+      {!showImage && (
+        <>
+          <Header />
+          <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+            <Hero />
+            <Benefits />
+            <Collaboration />
+            <Services />
+            <Pricing />
+            <Roadmap />
+          </div>
+          <ButtonGradient />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
